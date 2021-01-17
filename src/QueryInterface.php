8b29858e3e9e37db6ee8f6ab86d203272320188f
Anton Ukhanev <xedin.unknown@gmail.com>
2021-01-17 19:59:51 +0100

    Remove redundant methods from `QueryInterface`

--- src/QueryInterface.php
+++ src/QueryInterface.php
@@ -25,16 +25,6 @@ interface QueryInterface
     public function getResults(): iterable;
 
     /**
-     * Creates a new query with the specified param.
-     *
-     * @param string $name The name of the parameter.
-     * @phpcsSuppress SlevomatCodingStandard.TypeHints.ParameterTypeHint.MissingAnyTypeHint
-     * @param ?scalar|Stringable $value The value of the parameter
-     * @return self A new instance of this class, with the specified param added.
-     */
-    public function withParam(string $name, $value): QueryInterface;
-
-    /**
      * Creates a new query with only the specified params.
      *
      * @phpcsSuppress SlevomatCodingStandard.TypeHints.ParameterTypeHint.MissingTraversableTypeHintSpecification
@@ -42,12 +32,4 @@ interface QueryInterface
      * @return self A new instance of this class, with only the specified params.
      */
     public function withParams(array $params = []): QueryInterface;
-
-    /**
-     * Creates a new query without the specified params.
-     *
-     * @param list<string> $params The list of names for params to remove.
-     * @return self A new instance of this class, without the specified params.
-     */
-    public function withoutParams(array $params): QueryInterface;
 }
